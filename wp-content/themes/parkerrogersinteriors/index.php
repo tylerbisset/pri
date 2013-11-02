50b73ef8230c2c6c9cfe5a107c2e63cbc2d3603d
tylerbisset <tyler@tylerbisset.com>
2013-11-02 12:10:14 -0400

    Gallery update
    Working images on gallery. About and contact pages updated. Custom post
    type created but pretty permalinks not working.

--- wp-content/themes/parkerrogersinteriors/index.php
+++ wp-content/themes/parkerrogersinteriors/index.php
@@ -6,6 +6,6 @@ require_once(dirname(__FILE__).'/mvc/controllers/site_controller.php');
 $siteController = new SiteController();
 
 //call the showHome function
-$siteController->showHome();
+$siteController->showProjects();
 
 ?>
